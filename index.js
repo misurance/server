b16a7d88802f8f7b8a879818abadf87b9ac71096
yshay <yshay@soluto.com>
2015-10-08 13:12:18 +0300

    add base logic

--- index.js
+++ index.js
@@ -1,23 +1,61 @@
 var io = require('socket.io')(process.env.PORT || 5000);
+var r = require('rethinkdb');
+
+var options = {
+    host: 'ec2-52-17-218-63.eu-west-1.compute.amazonaws.com',
+    port: 28015,
+    db: 'misurance',
+    authKey: 'mHZ5dkYYkvBwdmKX',
+    timeout: 40
+};
+
+var rethinkdbConnection;
+
+r.connect(options).then(function(connection) {
+	rethinkdbConnection = connection;
+});
+
+var trafficDataTable = r.table('TrafficData');
+
 
 io.on('connection', function(socket){
   var loggedInUser = null;
-  socket.on('login', (username)=>
-    loggedInUser = username
-  );
+  var currentRide = null;
 
-  socket.on('location update', (time,location)=>{
+  var trafficDataStream = trafficDataTable
+  	.filter({ride:loggedInUser})
+  	.changes().run(rethinkdbConnection);
 
+  Rx.Node.fromStream(trafficDataStream)
+  	.subscribe((x)=>{
+  		console.log(x);
+  	});
+  
+
+  socket.on('start driving', (username, rideId)=>{
+  	 loggedInUser = username;
+  	 currentRide = rideId;
   });
 
-  socket.on('speed update', (time, speed)=>{
-
+  socket.on('position update', (time, speed, location)=>{
+  		trafficDataTable.insert({
+		    user:loggedInUser,
+		    eventType: 'position', 
+		    rideId,
+		    time,
+		    speed,
+		    location,
+		})
   });
 
   socket.on('emergency brake', (time, severity)=>{
-
+  		//update rethinkdb
+  		//update firebase on break
   });
 
-
+  socket.on('stop driving', (rideId)=>{
+  		//update rethinkdb
+  		//update firebase current speed
+  });
 
 });
