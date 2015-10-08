684ebec179aad0d3a5d79172d23c4da51faff86f
Oded Welgreen <oded.welgreen@gmail.com>
2015-10-08 13:59:23 +0300

    Get token endpoint

--- api.js
+++ api.js
@@ -1,6 +1,21 @@
 module.exports = function(app){
+  var braintree = require("braintree");
 
-  app.get('/api/token', function(req, res){
-    res.send('<h1>Hello world</h1>');
+  var gateway = braintree.connect({
+    environment: braintree.Environment.Sandbox,
+    merchantId: "w7vpqnt9bfgb68hr",
+    publicKey: "pv356yjm6zxhfcgf",
+    privateKey: "c0b1d94d789c8c5c446b7a5d6778ff7a"
+  });
+
+  app.get("/api/client_token", function (req, res) {
+    gateway.clientToken.generate({}, function (err, response) {
+      res.send(response.clientToken);
+    });
+  });
+
+  app.post("/checkout", function (req, res) {
+    var nonce = req.body.payment_method_nonce;
+    // Use payment method nonce here
   });
 };
