a19f9d64793861e8bd274b4e19fb693dfce2f0ef
Oded Welgreen <oded.welgreen@gmail.com>
2015-10-09 02:21:27 +0300

    fake gps data

--- testEvents.js
+++ testEvents.js
@@ -1,18 +1,24 @@
 var Firebase = require("firebase");
-var ref = new Firebase("https://intense-inferno-8553.firebaseio.com").child("events");
-var messages = [
-  'Entered dangerous area',
-  'Accident happened nearby',
-  'Exited dangerous area',
-  'Over the speed limit'
-]
+var ref = new Firebase("https://intense-inferno-8553.firebaseio.com").child("active");
+var drivers = [];
+var num = 5;
+for (var i = 0; i < num; i++) {
+  drivers[i] = {
+    key : "test" + i,
+    location:{
+    latitude:32.0667,
+    longitude:34.8000
+    }
+  };
+};
 setInterval(function(){
-  var date = new Date();
-  var event = {
-   time: date.getHours() + ':' + date.getMinutes(),
-   message: messages[Math.floor((Math.random() * messages.length))],
-   change: Math.floor((Math.random() * 3) - 1 )
- }
+  for (var i = 0; i < num; i++) {
+    drivers[i].location.latitude += 0.001 *  Math.floor((Math.random() * 3) - 1 );
+    drivers[i].location.longitude += 0.001 *  Math.floor((Math.random() * 3) - 1 );
+    ref.child(drivers[i].key).update({
+      location: drivers[i].location
+    })
+  }
+
  // console.log(event);
-  ref.push().set(event);
-},2500);
+},300);
